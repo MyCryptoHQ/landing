0d962e1e161118c20c8b21808a46bf142a08b21d
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-05-06 17:02:41 +0200

    Named aliases for jest

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -2,26 +2,34 @@
 const path = require('path');
 
 module.exports = {
-  roots: [path.resolve(`<rootDir>`, '../', 'src')],
+  rootDir: '../',
+  roots: ['src'],
   transform: {
-    '^.+\\.[jt]sx?$': `<rootDir>/jest-preprocess.js`,
+    '^.+\\.[jt]sx?$': `<rootDir>/__tests__/jest-preprocess.js`,
   },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     // A map from regular expressions to module names that allow to stub out resources with a single module
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file.mock.js`,
-    '^@/(.*)$': path.resolve('<rootDir', '../', 'src', '$1'),
-    gatsby: '<rootDir>/__mocks__/gatsby',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__tests__/__mocks__/file.mock.js`,
+    '@/components': path.resolve('src/components'),
+    '@/assets': path.resolve('src/assets'),
+    '@/pages': path.resolve('src/pages'),
+    '@/config': path.resolve('src/config'),
+    '@/types': path.resolve('src/types'),
+    '@/theme': path.resolve('src/theme'),
+    gatsby: '<rootDir>/__tests__/__mocks__/gatsby',
   },
   testPathIgnorePatterns: [
     `node_modules`,
     `\\.cache`,
-    path.resolve('<rootDir>', '../', '.*/public'),
+    path.resolve('<rootDir>', '.*/public'),
   ],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
   testURL: `http://localhost`,
-  setupFiles: [`<rootDir>/loadershim.js`],
+  setupFiles: [`<rootDir>/__tests__/loadershim.js`],
 };
