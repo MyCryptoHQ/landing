5a7fba7ec3d17e6b823e7e2af1ffa5e838869aa4
Guillaume Roux <guillaumeroux123@gmail.com>
2020-08-04 15:46:40 +0200

    add periods to Peace Of Mind section

--- src/components/PeaceOfMind/config.ts
+++ src/components/PeaceOfMind/config.ts
@@ -9,11 +9,11 @@ export const PEACE_OF_MIND: TPeaceOfMind[] = [
   {
     icon: 'vault',
     text:
-      "Get your information out of other people's hands and back into yours",
+      "Get your information out of other people's hands and back into yours.",
   },
   {
     icon: 'protect',
-    text: 'Protect yourself from phishing attacks and malicious extensions',
+    text: 'Protect yourself from phishing attacks and malicious extensions.',
   },
   {
     icon: 'vault',
