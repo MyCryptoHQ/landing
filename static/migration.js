58b4a33ad62b47c1bb631c07c4fd28cb0a7884f3
Frederik Bolding <frederik.bolding@gmail.com>
2020-08-11 13:34:14 +0200

    Wrapped code in a scope

--- static/migration.js
+++ static/migration.js
@@ -1,27 +1,29 @@
-const iframeSource = 'https://mycryptobuilds.com/pr/3415/';
+{
+  const iframeSource = 'https://mycryptobuilds.com/pr/3415/';
 
-// Create the iframe
-const iframe = document.createElement('iframe');
-iframe.setAttribute('src', iframeSource);
-iframe.setAttribute('id', 'the_iframe');
-iframe.style.width = 450 + 'px';
-iframe.style.height = 200 + 'px';
-document.body.appendChild(iframe);
+  // Create the iframe
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('src', iframeSource);
+  iframe.setAttribute('id', 'the_iframe');
+  iframe.style.width = 450 + 'px';
+  iframe.style.height = 200 + 'px';
+  document.body.appendChild(iframe);
 
-// Send a message to the child iframe
-const iframeEl = document.getElementById('the_iframe');
+  // Send a message to the child iframe
+  const iframeEl = document.getElementById('the_iframe');
 
-// Send a message to the child iframe
-const sendMessage = function(msg) {
-  // Make sure you are sending a string, and to stringify JSON
-  iframeEl.contentWindow.postMessage(msg, '*');
-};
+  // Send a message to the child iframe
+  const sendMessage = function(msg) {
+    // Make sure you are sending a string, and to stringify JSON
+    iframeEl.contentWindow.postMessage(msg, '*');
+  };
 
-iframe.addEventListener('load', function() {
-  const data = window.localStorage.getItem('MYC_Storage');
-  if (data) {
-    sendMessage({ type: 'migration', data });
-  }
-  // Delete to not trigger the migration again
-  window.localStorage.removeItem('MYC_Storage');
-});
+  iframe.addEventListener('load', function() {
+    const data = window.localStorage.getItem('MYC_Storage');
+    if (data) {
+      sendMessage({ type: 'migration', data });
+    }
+    // Delete to not trigger the migration again
+    window.localStorage.removeItem('MYC_Storage');
+  });
+}
