6aee08e2db8673de7fff74985d678c043e01b2de
Guillaume Roux <guillaumeroux123@gmail.com>
2020-08-11 14:53:05 +0200

    remove useless import

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { useStaticQuery, graphql, withPrefix } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 function SEO({
   description = '',
