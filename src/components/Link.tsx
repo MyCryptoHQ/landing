240cd2cfe6cd7108003a184a42b9638c9404cb55
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-01-25 12:51:02 +0100

    Remove expect-error

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,8 +4,7 @@ import { LinkProps, Link as ExtLink } from 'rebass';
 
 const Link = ({ href, ...props }: LinkProps & { internal?: boolean }) => {
   if (props.internal && href) {
-    // @ts-expect-error: Rebass & Gatsby conflict
-    return <GatsbyLink to={href} {...props} />;
+    return <GatsbyLink to={href}>{props.children}</GatsbyLink>;
   }
   return <ExtLink {...props} href={href} target="_blank" rel="noreferrer" />;
 };
