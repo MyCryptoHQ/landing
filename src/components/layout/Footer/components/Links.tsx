39680e0f0fd4c54b674661bfb92cdc60afe65cd1
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-01-26 19:22:23 +0100

    Remove self link

--- src/components/layout/Footer/components/Links.tsx
+++ src/components/layout/Footer/components/Links.tsx
@@ -15,10 +15,6 @@ export const LINK_COLUMNS: TLink[] = [
     heading: 'Company',
     links: [
       {
-        title: 'MyCrypto.com',
-        url: 'https://www.mycrypto.com/',
-      },
-      {
         title: 'Help & Support',
         url: 'https://support.mycrypto.com/',
       },
