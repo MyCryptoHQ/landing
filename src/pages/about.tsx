42be01c45345fb085dde2e7caa274e8d65d386b1
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-01-26 20:56:51 +0100

    Add brief

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -271,6 +271,10 @@ const TEAM = [
   },
 ];
 
+// eslint-disable-next-line
+const Brief =
+  'MyCrypto is an open-source, client-side tool for interacting with the blockchain. Developed by and for the community beginning in 2015, we’re focused on building awesome products that put the power in people’s hands.';
+
 const About = () => <ComingSoon />;
 
 export default About;
