faddc3deb3a034a5893e8ee738f02bdb26de5d10
Guillaume Roux <guillaumeroux123@gmail.com>
2021-01-26 19:03:45 +0100

    add styling to gatsby links

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 import { LinkProps, Link as ExtLink } from 'rebass';
+import styled from 'styled-components';
+import {
+  layout,
+  space,
+  fontSize,
+  color,
+  flex,
+  order,
+  alignSelf,
+} from 'styled-system';
+
+const SGatsbyLink = styled(GatsbyLink)<LinkProps>`
+  ${space}
+  ${layout}
+  ${fontSize}
+  ${color}
+  ${flex}
+  ${order}
+  ${alignSelf}
+`;
 
 const Link = ({ href, ...props }: LinkProps & { internal?: boolean }) => {
   if (props.internal && href) {
-    return <GatsbyLink to={href}>{props.children}</GatsbyLink>;
+    return (
+      <SGatsbyLink to={href} style={{ textDecoration: 'none' }} {...props}>
+        {props.children}
+      </SGatsbyLink>
+    );
   }
   return <ExtLink {...props} href={href} target="_blank" rel="noreferrer" />;
 };
