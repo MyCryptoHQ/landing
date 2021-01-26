1cee085c1a76f97e1903698750f46749c3fd68a9
Emmanuel Vilsbol <emmanuel@mycrypto.com>
2021-01-26 21:19:51 +0100

    Copy content from `about.mycrypto.com` (#54)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { ComingSoon } from '@components';
 
+// eslint-disable-next-line
+const Brief =
+  'MyCrypto is an open-source, client-side tool for interacting with the blockchain. Developed by and for the community beginning in 2015, we’re focused on building awesome products that put the power in people’s hands.';
+
 const About = () => <ComingSoon />;
 
 export default About;
