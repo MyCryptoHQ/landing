5a397b0fe29e4a9e698b707c0a1157890517b62d
Guillaume Roux <guillaumeroux123@gmail.com>
2021-11-18 02:39:30 +0100

    Add links to bottom actions

--- src/config/actions.ts
+++ src/config/actions.ts
@@ -1,4 +1,5 @@
 import { Action } from '@types';
+import { getRoute } from '@utils';
 
 export const ACTION_CARDS: Action[] = [
   {
@@ -6,13 +7,13 @@ export const ACTION_CARDS: Action[] = [
     title: 'Taking your first steps?',
     text: 'You’ve come to the right place. Create a wallet and start your crypto journey now!',
     button: 'Create Wallet',
-    link: 'https://exemple.com',
+    link: getRoute('DOWNLOAD'),
   },
   {
     icon: 'connect-wallet',
     title: 'Already have ETH?',
     text: 'You know why you’re here! Connect your wallet(s) now.',
     button: 'Connect Wallet',
-    link: 'https://exemple.com',
+    link: getRoute('ADD_ACCOUNT'),
   },
 ];
